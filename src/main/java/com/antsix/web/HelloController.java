f64e20f55c18cc5cd4154f8bec617b6d56e26902
yhZhao <yhzhaohy@163.com>
2019-07-22 13:50:59 +0800

    0722

--- src/main/java/com/antsix/web/HelloController.java
+++ src/main/java/com/antsix/web/HelloController.java
@@ -47,7 +47,7 @@ public class HelloController {
         // 加入一个属性，用来在模板中读取
         map.addAttribute("host","http://antsix.com");
         // return模板文件的名称，对应src/main/resources/templates/index.html
-        return "index";
+        return "admin/testSql";
     }
 
 }
